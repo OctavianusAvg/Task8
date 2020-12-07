9d33c576d2e23b4a9609fe2f8323783a1dca5f04
Ruslan Kanuka <netlogina7@gmail.com>
2020-12-08 01:21:59 +0200

    Task8

---
+++ Task8/MyComplex.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task8
+{
+    class MyComplex
+    {
+        private double Re, Im;
+
+        public MyComplex(double InitRe = 0, double InitIm = 0)
+        {
+            Re = InitRe;
+            Im = InitIm;
+        }
+
+        public static MyComplex operator +(MyComplex a, double b)
+        {
+            MyComplex num = new MyComplex();
+            num.Re = a.Re + b;
+            num.Im = a.Im;
+            return num;
+        }
+
+        public static MyComplex operator +(double a, MyComplex b)
+        {
+            return b + a;
+        }
+
+        public static MyComplex operator +(MyComplex a, MyComplex b)
+        {
+            MyComplex num = new MyComplex();
+            num.Re = a.Re + b.Re;
+            num.Im = a.Im + b.Im;
+            return num;
+        }
+
+        public static MyComplex operator -(MyComplex a, MyComplex b)
+        {
+            MyComplex num = new MyComplex();
+            num.Re = a.Re - b.Re;
+            num.Im = a.Im - b.Im;
+            return num;
+        }
+
+        public static MyComplex operator -(MyComplex a)
+        {
+            MyComplex num = new MyComplex();
+            num.Re = -1 * a.Re;
+            num.Im = a.Im;
+            return num;
+        }
+        // Варіант 4 
+        // Операція "*" (A+Bi)*(C+Di) = (A*C) + (B*D)i
+        public static MyComplex operator *(MyComplex a, MyComplex b)
+        {
+            MyComplex num = new MyComplex();
+            num.Re = a.Re * b.Re;
+            num.Im = a.Im * b.Im;
+            return num;
+        }
+        public void InputFromTerminal()
+        {
+            string s;
+            double real, imagine;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введіть реальну частину");
+                    real = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введіть уявну частину");
+                    imagine = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Не правильні данні введенно. Повторення.");
+                    continue;
+                }
+                Re = real;
+                Im = imagine;
+                break;
+            } while (true);
+        }
+        public double this[string type]
+        {
+            get
+            {
+                if (type == "imageValue")
+                {
+                    return Im;
+                }
+                else
+                {
+                    return Re;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $" {Re} + {Im}*Im або {Re} - {Im}*Im";
+        }
+
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+            MyComplex A = new MyComplex(1, 1), B = new MyComplex(), C = new MyComplex(1), D = new MyComplex();
+            C = A + B;
+            C = A + 10.5;
+            C = 10.5 + A;
+            D = -C;
+            C = A + B + C + D;
+            C = A = B = C;
+            // Введення А 
+            D.InputFromTerminal();
+            // Виведення комплексного числа в форматі 5+7 *Im або 5-7*Im 
+            Console.WriteLine($"A = {A}, B = {B}, C = {C}, D = {D}");
+            // Виведення комплексного числа в форматі 
+            Console.WriteLine($"Re(A) = {A["realValue"]}, Im(A) = {A["imageValue"]}");
+            Console.ReadLine();
+        }
+    }
+}
